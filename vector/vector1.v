0a9075bc0f5b9e3fc615e46f0049fb4b28e10f64
jagan-codes <jagan15092005@gmail.com>
2024-06-28 18:18:02 +0530

    added

--- vector/vector1.v
+++ vector/vector1.v
@@ -1,4 +1,4 @@
-1:
+
 module top_module (
     input [4:0] a, b, c, d, e, f,
     output [7:0] w, x, y, z );//
@@ -6,44 +6,8 @@ module top_module (
     assign {w,x,y,z} = {a,b,c,d,e,f,1'b1,1'b1};
 
 endmodule
-2:
-module top_module( 
-    input [7:0] in,
-    output [7:0] out
-);
-    assign out[0]=in[7];
-    assign out[1]=in[6];
-    assign out[2]=in[5];
-    assign out[3]=in[4];
-    assign out[4]=in[3];
-    assign out[5]=in[2];
-    assign out[6]=in[1];
-    assign out[7]=in[0];
 
-endmodule
-3:
-module top_module (
-    input [7:0] in,
-    output [31:0] out );//
 
-    assign out = {24{in[7]} , in };
 
-endmodule
-
-4:
-module top_module (
-	input a, b, c, d, e,
-	output [24:0] out
-);
-
-	wire [24:0] top, bottom;
-	assign top    = { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} };
-	assign bottom = {5{a,b,c,d,e}};
-	assign out = ~top ^ bottom;	// Bitwise XNOR
-
-	// This could be done on one line:
-	// assign out = ~{ {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ {5{a,b,c,d,e}};
-	
-endmodule
 
 
--- vector/vectors9.v
+++ vector/vectors9.v
@@ -11,51 +11,8 @@ assign outv=vec;
     assign o2=vec[2];
     
 endmodule
-2:
-`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
-module top_module( 
-    input wire [15:0] in,
-    output wire [7:0] out_hi,
-    output wire [7:0] out_lo );
-    assign out_lo=in[7:0];
-    assign out_hi=in[15:8];
-endmodule
-
-3:
-module top_module( 
-    input [31:0] in,
-    output [31:0] out );//
-    assign out[7:0]=in[31:24];
-    assign out[15:8]=in[23:16];
-    assign out[23:16]=in[15:8];
-    assign out[31:24]=in[7:0];
 
 
-endmodule
-4:
-module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
-);
-    assign out_or_bitwise=a|b;
-    assign out_or_logical=a||b;
-    assign out_not[2:0]=~a;
-    assign out_not[5:3]=~b;
-    
 
-endmodule
-5:
-module top_module( 
-    input [3:0] in,
-    output out_and,
-    output out_or,
-    output out_xor
-);
-assign out_and=&in;
-    assign out_or=|in;
-    assign out_xor=^in;
-endmodule
+
 
